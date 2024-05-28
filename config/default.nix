a5cd0984da37334ffc11664dd2ae84fdd84a80f6
kevinlmadison <coolklm121@gmail.com>
2024-05-28 15:07:50 -0400

    force osc52

--- config/default.nix
+++ config/default.nix
@@ -217,4 +217,17 @@ in {
       zig
     ];
   };
+  extraConfigLua = ''
+    vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+      },
+      paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+      },
+    }
+  '';
 }
