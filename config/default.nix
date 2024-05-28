935bcc088dd35d4e4904639b647f03ce60195937
kevinlmadison <coolklm121@gmail.com>
2024-05-28 15:09:24 -0400

    wrong section

--- config/default.nix
+++ config/default.nix
@@ -216,18 +216,18 @@ in {
       # tmux-sessionizer
       zig
     ];
+    extraConfigLua = ''
+      vim.g.clipboard = {
+        name = 'OSC 52',
+        copy = {
+          ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+          ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+        },
+        paste = {
+          ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+          ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+        },
+      }
+    '';
   };
-  extraConfigLua = ''
-    vim.g.clipboard = {
-      name = 'OSC 52',
-      copy = {
-        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-      },
-      paste = {
-        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-      },
-    }
-  '';
 }
